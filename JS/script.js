0f5e62062430c238c5fe8918918d4cb4674500a3
Rudenko Mykhailo <fernandosolotop@gmail.com>
2023-11-30 17:11:06 +0200

    grid is too strong

--- JS/script.js
+++ JS/script.js
@@ -1,86 +1,157 @@
-let password = document.querySelector('#password');
-let passwordShow = document.querySelector('.password_show');
 // show password
-passwordShow.onclick = function(){
+const inputPasswords = document.querySelectorAll('.password');
+const showPasswordButtons = document.querySelectorAll('.show__password');
 
-    if(password.type === 'password'){
-        password.type = 'text';
-    }else{
-        password.type = 'password';
-    }
-    
-}
-// checking password
-function passwordCheck(pass){
-    let difficultScore = 0;
-    if (pass.length > 6) difficultScore++;
-    if (pass.length >= 10) difficultScore++;
-    if (pass.match(/[a-z]/)) difficultScore++;
-    if (pass.match(/[A-Z]/)) difficultScore++;
-    if (pass.match(/[0-9]/)) difficultScore++;
-    if (pass.match(/[!@#$%^&*()]/)) difficultScore++;
-    return difficultScore;
-}
-
-// checking password glow line
-let form = document.querySelector('.login_page_form');
-
-document.addEventListener('keyup', function(e){
-
-    let password = document.querySelector('#password').value;
-    let check = passwordCheck(password);
-    if (check === 0) {
-        form.classList.remove('easy');
-        form.classList.remove('medium');
-        form.classList.remove('strong');
-    }else if (check < 3 && check > 0) {
-        form.classList.add('easy');
-        form.classList.remove('medium');
-        form.classList.remove('strong');
-    } else if (check < 5 && check > 2) {
-        form.classList.add('medium');
-        form.classList.remove('easy');
-        form.classList.remove('strong');
-    } else if (check >= 5) {
-        form.classList.add('strong');
-        form.classList.remove('easy');
-        form.classList.remove('medium');
-    }
+showPasswordButtons.forEach((button, index) =>{
+button.addEventListener('click', () => {
+    if (inputPasswords[index].type === 'password') inputPasswords[index].type = 'text';
+    else inputPasswords[index].type = 'password';
+})
 })
 
-//login/register animation
+// passcheck
+const passwordCheck = document.querySelector('.password__check');
 
-const linkToLogin = document.querySelector('.login_link');
-const registerFormn = document.querySelector('#register__form')
 
-linkToLogin.addEventListener('click', function(){
-    hideRegisterForm();
+
+function passCheck(password){
+    let score = 0;
+    if ( password.length > 0) score++;
+    if (password.length > 6) score++;
+    if (/[a-z]/.test(password)) score++;
+    if (/[A-Z]/.test(password)) score++;
+    if (/[0-9]/.test(password)) score++;
+    if (/[!@#$%^&*()]/.test(password)) score++;
+    return score;
+}
+
+
+document.addEventListener('keyup', () => {
+    let passScore = passCheck(document.querySelector('#password_register').value);
+    // alert (passScore)
+    if (passScore === 0) {
+        passwordCheck.style.width = '0%';
+        passwordCheck.style.backgroundColor = 'none';
+        passwordCheck.style.boxShadow = 'none'
+    }
+    else if (passScore < 3 && passScore !== 0) {
+        passwordCheck.style.width = '33%';
+        passwordCheck.style.backgroundColor = 'rgb(255, 0, 0)'
+        passwordCheck.style.boxShadow = '0 0 10px 3px rgb(255, 0, 0)'
+    } else if (passScore <= 5){
+        passwordCheck.style.width = '66%'
+        passwordCheck.style.backgroundColor = 'rgb(13, 0, 255)'
+        passwordCheck.style.boxShadow = '0 0 10px 3px rgb(13, 0, 255)'
+    } else if (passScore > 5){
+        passwordCheck.style.width = '100%'
+        passwordCheck.style.backgroundColor = 'rgb(38, 255, 0)'
+        passwordCheck.style.boxShadow = '0 0 10px 3px rgb(38, 255, 0)'
+    }
+
 })
-// i need to remale all this code :)
-function hideRegisterForm(){
-    linkToLogin.style.opacity = '0';
-    registerFormn.style.height = '3px';
-    registerFormn.style.padding = '0';
-    setTimeout(() => {registerFormn.style.width = '30px';}, 400);
-    setTimeout(() => {
-        registerFormn.style.width = '350px';
-        registerFormn.style.left = '-20%';
-    }, 1000);
-    setTimeout(() => {
-        registerFormn.style.opacity = '0';
-    }, 1500);
-}
 
-function showLoginForm(){
-    linkToLogin.style.opacity = '0';
-    registerFormn.style.height = '3px';
-    registerFormn.style.padding = '0';
-    setTimeout(() => {registerFormn.style.width = '30px';}, 400);
+//
+
+
+//form out animation
+
+const triggerLinkLoginToRegister = document.querySelector('#link_login_to_register');
+const loginForm = document.querySelector('#login__form')
+
+const triggerLinkRegisterToLogin = document.querySelector('#link_register_to_login')
+const registerForm = document.querySelector('#register__form')
+
+const password_register = document.querySelector('#password_register')
+const email_register = document.querySelector('#emailRegister')
+
+
+triggerLinkLoginToRegister.addEventListener('click', () => {
+        // triggerLinkLoginToRegister.style.opacity = '0';
+        loginForm.style.padding = "0";
+        loginForm.style.height = '3px';
+        loginForm.style.overflow = 'hidden';
+        setTimeout(() => {
+            loginForm.style.width = '80px';
+            // loginForm.style.backgroundColor = 'aliceblue';
+        }, 200);
+        setTimeout(() => {
+            loginForm.style.width = '150px';
+            loginForm.style.left = '-20%';
+        }, 500);
+        setTimeout(() => {
+            loginForm.style.display = 'none';
+        }, 1000);
+        setTimeout(() => {
+            loginForm.style.left = '120%';
+        }, 1100);
+
+
+        //register card arrive
+        registerForm.style.display = 'flex';
+        password_register.setAttribute('required','');
+        email_register.setAttribute('required','');
     setTimeout(() => {
-        registerFormn.style.width = '350px';
-        registerFormn.style.left = '-20%';
-    }, 1000);
+
+        // registerForm.style.width = '150px';
+        registerForm.style.left = '50%';
+        setTimeout(() => {
+            registerForm.style.width = '20%';
+            // registerForm.style.backgroundColor = 'aliceblue';
+        }, 300);
+        setTimeout(() => {
+            registerForm.style.padding = "1% 1%";
+            registerForm.style.height = '40%';
+            registerForm.style.overflow = 'unset';
+        }, 500);
+
+    }, 1000)
+
+    })
+
+
+//register card moves off
+    triggerLinkRegisterToLogin.addEventListener('click', () => {
+        password_register.removeAttribute('required');
+        email_register.removeAttribute('required');
+        // triggerLinkLoginToRegister.style.opacity = '0';
+        registerForm.style.padding = "0";
+        registerForm.style.height = '3px';
+        registerForm.style.overflow = 'hidden';
+        setTimeout(() => {
+            registerForm.style.width = '80px';
+            // registerForm.style.backgroundColor = 'aliceblue';
+        }, 200);
+        setTimeout(() => {
+            registerForm.style.width = '150px';
+            registerForm.style.left = '-20%';
+        }, 500);
+        setTimeout(() => {
+            registerForm.style.display = 'none';
+        }, 1000);
+        setTimeout(() => {
+            registerForm.style.left = '120%';
+        }, 1100);
+
+
+        //login card arrive
+        loginForm.style.display = 'flex';   
     setTimeout(() => {
-        registerFormn.style.opacity = '0';
-    }, 1500);
-}
+
+        // registerForm.style.width = '150px';
+        loginForm.style.left = '50%';
+        setTimeout(() => {
+            loginForm.style.width = '20%';
+            // registerForm.style.backgroundColor = 'aliceblue';
+        }, 300);
+        setTimeout(() => {
+            loginForm.style.padding = "1% 1%";
+            loginForm.style.height = '40%';
+            loginForm.style.overflow = 'unset';
+        }, 500);
+
+    }, 1000)
+
+    })
+
+
+
