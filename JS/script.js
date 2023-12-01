703e3f915f7d44940d1956da7878d69dc5df8d23
Rudenko Mykhailo <fernandosolotop@gmail.com>
2023-12-01 17:40:23 +0200

    ы
    ы

--- JS/script.js
+++ JS/script.js
@@ -172,30 +172,72 @@ triggerLinkLoginToRegister.addEventListener('click', () => {
     let aRA;
 
   
-    function animation(){
-
-        for (let i = 1; i < 9999;){
-            setTimeout(() => {
-                LOL();
-                if (i === 29) i = 0;
-            }, 30 * ++i);
-            
-        }
-
-
-
-        function LOL(){
-            if (Number(gridBackgroundArray[1]) === 30){ 
-                gridBackgroundArray[1] = 0;
-                gridBackgroundArray[3] = 3;
-            }
-            gridBackgroundArray[1] = Number(gridBackgroundArray[1])+1;
-            gridBackgroundArray[3] = Number(gridBackgroundArray[3])+1;
-            aRA = gridBackgroundArray.join('');
-            syntwaveGrid.style.backgroundImage = aRA
-        }
-        
+    var start = null;
+    var element = document.querySelector('.body_syntwave-grid');
+    
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      var progress = timestamp - start;
+      element.style.transform =
+        "translateX(" + Math.min(progress / 10, 200) + "px)";
+      if (progress < 2000) {
+        window.requestAnimationFrame(step);
+      }
     }
     
-    // animation();
+    // window.requestAnimationFrame(step);
 
+//   animationGrid();
+
+
+
+
+    // visualizer
+
+    // const visualizerBtn = document.querySelector('.triangle');
+    // const audio = document.querySelector('.audio');
+    // const vizual = document.querySelector('.visual');
+
+    // visualizerBtn.addEventListener('click', e =>{
+    //     audio.paused ? audio.play() : audio.pause();
+    //     visualizerBtn.classList.toggle('btn-play');
+    //     visualizerBtn.classList.toggle('btn-pause');
+    // })
+
+
+
+    // window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    // const ctx = new window.AudioContext();
+    // const analyser = ctx.createAnalyser();
+    // const source = ctx.createMediaElementSource(audio);
+    // source.connect(analyser);
+    // source.connect(ctx.destination);
+    // analyser.fftSize = 32;
+    // const bufferLength = analyser.frequencyBinCount;  
+
+    // let dataArray = new Uint8Array(bufferLength);
+    // let elements = [];
+    // for (let i = 0; i < bufferLength / 2; i++){
+    //     const element = document.createElement('span');
+    //     element.classList.add('element');
+    //     elements.push(element);
+    //     vizual.appendChild(element);
+    // }
+
+    // const clamp = (num, min, max) => {
+    //     if (num >= max) return max;
+    //     if (num <= min) return min;
+    //     return num
+    // }
+
+    // const update = () => {
+    //     requestAnimationFrame(update);
+    //     analyser.getByteFrequencyData(dataArray);
+    //     for (let i = 0; i < bufferLength; i++){
+    //         let item = dataArray[i];
+    //         item = item < 200 ? item / 1.5 : item * 1.25;
+    //         elements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, ${clamp(item, 125, 150)}px)`;
+    //     }
+    // }
+    
+    // update();
