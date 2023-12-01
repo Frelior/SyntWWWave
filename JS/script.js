f15602d30015d3d33caf0ac66ee5e457e8f255da
Rudenko Mykhailo <fernandosolotop@gmail.com>
2023-12-01 10:35:15 +0200

    made animation, but
    but turned it off. cause it takes too much cpu

--- JS/script.js
+++ JS/script.js
@@ -90,17 +90,20 @@ triggerLinkLoginToRegister.addEventListener('click', () => {
         registerForm.style.display = 'flex';
         password_register.setAttribute('required','');
         email_register.setAttribute('required','');
+        registerForm.style.width = '80px';
     setTimeout(() => {
 
-        // registerForm.style.width = '150px';
+        registerForm.style.width = '150px';
         registerForm.style.left = '50%';
         setTimeout(() => {
-            registerForm.style.width = '20%';
+            
+            registerForm.style.width = '300px';
             // registerForm.style.backgroundColor = 'aliceblue';
         }, 300);
         setTimeout(() => {
+            
             registerForm.style.padding = "1% 1%";
-            registerForm.style.height = '40%';
+            registerForm.style.height = '300px';
             registerForm.style.overflow = 'unset';
         }, 500);
 
@@ -140,12 +143,12 @@ triggerLinkLoginToRegister.addEventListener('click', () => {
         // registerForm.style.width = '150px';
         loginForm.style.left = '50%';
         setTimeout(() => {
-            loginForm.style.width = '20%';
+            loginForm.style.width = '300px';
             // registerForm.style.backgroundColor = 'aliceblue';
         }, 300);
         setTimeout(() => {
             loginForm.style.padding = "1% 1%";
-            loginForm.style.height = '40%';
+            loginForm.style.height = '300px';
             loginForm.style.overflow = 'unset';
         }, 500);
 
@@ -153,5 +156,46 @@ triggerLinkLoginToRegister.addEventListener('click', () => {
 
     })
 
+  
 
+    // linear-gradient(90deg, rgba(0, 0, 0, 0) 0px
+    // linear-gradient(180deg, rgba(0, 0, 0, 0) 0px, rgba(54, 226, 248, 0.5) 0%, rgba(39, 230, 255, 0.908) 3px, rgba(0, 0, 0, 0) 0px), linear-gradient(90deg, rgba(0, 0, 0, 0) 0px, rgba(54, 226, 248, 0.5) 0%, rgba(39, 230, 255, 0.908) 3px, rgba(0, 0, 0, 0) 0px);
+
+
+    // const gridBackground = window.getComputedStyle(syntwaveGrid).background;  // узнай что это такое!
+    // const gradientRegex = /linear-gradient\(\d+deg,.*?\)/g; //поиск строки linear-gradient(180deg, rgba(0, 0, 0, 0) 0px
+    // const gragients = gridBackground.match(gradientRegex);
+
+    const syntwaveGrid = document.querySelector('.body_syntwave-grid');
+  //[1] [3]  подсказочка по индексам градиентов, они будут меняться и возвращаться строкой
+    let gridBackgroundArray = ['linear-gradient(180deg, rgba(0, 0, 0, 0) ', '0', 'px, rgba(54, 226, 248, 0.5) 0%, rgba(39, 230, 255, 0.908) ', '3', 'px, rgba(0, 0, 0, 0) 0px), linear-gradient(90deg, rgba(0, 0, 0, 0) 0px, rgba(54, 226, 248, 0.5) 0%, rgba(39, 230, 255, 0.908) 3px, rgba(0, 0, 0, 0) 0px)'];
+    let aRA;
+
+  
+    function animation(){
+
+        for (let i = 1; i < 9999;){
+            setTimeout(() => {
+                LOL();
+                if (i === 29) i = 0;
+            }, 30 * ++i);
+            
+        }
+
+
+
+        function LOL(){
+            if (Number(gridBackgroundArray[1]) === 30){ 
+                gridBackgroundArray[1] = 0;
+                gridBackgroundArray[3] = 3;
+            }
+            gridBackgroundArray[1] = Number(gridBackgroundArray[1])+1;
+            gridBackgroundArray[3] = Number(gridBackgroundArray[3])+1;
+            aRA = gridBackgroundArray.join('');
+            syntwaveGrid.style.backgroundImage = aRA
+        }
+        
+    }
+    
+    // animation();
 
